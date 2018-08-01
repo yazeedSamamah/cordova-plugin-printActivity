39d0c0825f0ee6e2bd46456fbdab1fa46c5e52ca
yazeedSamamah <yazeed_samamah@yahoo.com>
2018-08-01 17:59:17 +0300

    add all

---
+++ www/printActivity.js
@@ -0,0 +1,9 @@
+var exec = require('cordova/exec');
+
+var Printer = {
+   print: function(fnSuccess, fnError, str){
+      exec(fnSuccess, fnError, "BluetoothPrinter", "print", [str]);
+   }
+};
+
+module.exports = Printer;
