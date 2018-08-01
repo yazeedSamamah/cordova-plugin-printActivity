a1da3b9adaa677ee4613247754f85d35d270052c
DESKTOP-5EFESU7\Hos-Gh <hosam.algharabli@aya-pay.com>
2018-08-01 16:23:24 +0300

    rm all

--- www/printActivity.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-var Printer = {
-   print: function(fnSuccess, fnError, str){
-      exec(fnSuccess, fnError, "BluetoothPrinter", "print", [str]);
-   }
-};
-
-module.exports = Printer;
