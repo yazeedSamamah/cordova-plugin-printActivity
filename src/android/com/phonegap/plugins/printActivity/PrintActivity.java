e2561160118bd52bea3bccd29b8691361e5c9ffb
yazeedSamamah <yazeed_samamah@yahoo.com>
2018-08-02 17:19:56 +0300

    update java file

--- src/android/com/phonegap/plugins/printActivity/PrintActivity.java
+++ src/android/com/phonegap/plugins/printActivity/PrintActivity.java
@@ -118,6 +118,7 @@ public class PrintActivity extends CordovaPlugin {
 
         printThread = new Print_Thread(PRINT_TEST);
         printThread.start();
+          throw new PrintInitException("I am Exception Alpha!");
            } catch (PrintInitException e) {
                Log.e(tag, e.getMessage());
                e.printStackTrace();
