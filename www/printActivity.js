1ca342f1fd38480969dcc02aaf2955097d007678
yazeedSamamah <yazeed_samamah@yahoo.com>
2018-08-04 20:51:56 +0300

    update java file

--- www/printActivity.js
+++ www/printActivity.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.print = function( str, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'PrintActivity', 'print', [str]);
+exports.print = function(mac, str, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'PrintActivity', 'print', [mac, str]);
 };
