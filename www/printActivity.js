fb3d00f94b496ede992793b4a7fb110dcd2d07d8
yazeedSamamah <yazeed_samamah@yahoo.com>
2018-08-13 14:53:06 +0300

    Update printActivity.js

--- www/printActivity.js
+++ www/printActivity.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.print = function( str, successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, 'PrintActivity', 'print', [str]);
+    cordova.exec(successCallback, errorCallback, 'PrintActivity', 'print', str);
 };
