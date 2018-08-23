0a13cc6e9aecf2a296783444c89f2115e8ef2444
yazeedSamamah <yazeed_samamah@yahoo.com>
2018-08-23 20:11:30 +0300

    Update PrintActivity.java

--- src/android/com/phonegap/plugins/printActivity/PrintActivity.java
+++ src/android/com/phonegap/plugins/printActivity/PrintActivity.java
@@ -469,7 +469,7 @@ public class PrintActivity extends CordovaPlugin {
                         BitmapFactory.decodeResource(PrintActivity.this.getResources(), 
                         R.drawable.ayaPay.bmp); 
                         ret = PosApiHelper.getInstance().PrintBmp(bmp); */
-                     String arabic = "عربي";
+                     String arabic = "الاردن";
 
                         posApiHelper.PrintSetFont((byte) 24, (byte) 24, (byte) 0x33);
                         posApiHelper.PrintStr("AYA PAY POS SALES SLIP\n");
@@ -498,7 +498,7 @@ public class PrintActivity extends CordovaPlugin {
                         posApiHelper.PrintStr("\n");
                         posApiHelper.PrintStr("\n");
                         posApiHelper.PrintStr("- -  CUSTOMER COPY  - -\n");
-                        posApiHelper.PrintStr(new String(arabic.getBytes("UTF-8")) + "\n");
+                        posApiHelper.PrintStr(new String(arabic.getBytes("UTF-8"),"UTF-8") + "\n");
                         //  posApiHelper.PrintStr("\n");
                         posApiHelper.PrintStr("                                         ");
                         posApiHelper.PrintStr("\n");
