edc7cb63f1e5c7fc5df39d820aba4190b73e7a52
yazeedSamamah <yazeed_samamah@yahoo.com>
2018-08-24 23:46:28 +0300

    we need to make this work

--- src/android/com/phonegap/plugins/printActivity/PrintActivity.java
+++ src/android/com/phonegap/plugins/printActivity/PrintActivity.java
@@ -512,7 +512,7 @@ public class PrintActivity extends CordovaPlugin {
                    //     SendMsg("Printing... ");mipmap
                     //fakeR = new FakeR(this);
                    
-          Bitmap bmp = BitmapFactory.decodeResource(cordova.getActivity().getResources(), R.mipmap.metrolinx1bitdepth);
+          Bitmap bmp = BitmapFactory.decodeResource(PrintActivity.this.cordova.getActivity().getResources(), R.mipmap.metrolinx1bitdepth);
                         ret = posApiHelper.PrintBmp(bmp);
                         posApiHelper.PrintStr("法语:Bonjour! Ça fait longtemps!\n");
                         posApiHelper.PrintStr("日语:こんにちは！久しぶり！\n");
