45dda2466351f97d8087f0d69784cede39b0ed58
yazeedSamamah <yazeed_samamah@yahoo.com>
2018-08-24 03:23:44 +0300

    add new image

--- src/android/com/phonegap/plugins/printActivity/PrintActivity.java
+++ src/android/com/phonegap/plugins/printActivity/PrintActivity.java
@@ -510,7 +510,7 @@ public class PrintActivity extends CordovaPlugin {
                         posApiHelper.PrintStr("\n");*/
 
                    //     SendMsg("Printing... ");
-                                                   Bitmap bmp = BitmapFactory.decodeResource(getResources(),R.drawable.large_icon);
+                                                   Bitmap bmp = BitmapFactory.decodeResource(PrintActivity.this.getResources(),R.drawable.large_icon);
                         ret = posApiHelper.PrintBmp(bmp);
                         posApiHelper.PrintStr("法语:Bonjour! Ça fait longtemps!\n");
                         posApiHelper.PrintStr("日语:こんにちは！久しぶり！\n");
