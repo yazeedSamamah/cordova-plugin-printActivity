71d39ac04e86c61413c0b014e303c1e4bb52cf6f
yazeedSamamah <yazeed_samamah@yahoo.com>
2018-08-30 00:54:33 +0300

    add new image

--- src/android/com/phonegap/plugins/printActivity/PrintActivity.java
+++ src/android/com/phonegap/plugins/printActivity/PrintActivity.java
@@ -68,52 +68,10 @@ import org.json.JSONException;
 public class PrintActivity extends CordovaPlugin {
 
       public String tag = "PrintActivity";
-    final int PRINT_TEST = 0;
-    final int PRINT_UNICODE = 1;
-    final int PRINT_BMP = 2;
-    final int PRINT_BARCODE = 4;
-    final int PRINT_CYCLE = 5;
-    final int PRINT_LONGER = 7;
-    final int PRINT_OPEN = 8;
-
-    private RadioGroup rg = null;
-    private Timer timer;
-    private Timer timer2;
-    private BroadcastReceiver receiver;
-    private IntentFilter filter;
-    private int voltage_level;
-    private int BatteryV;
-    SharedPreferences preferences;
-    SharedPreferences sp;
-    SharedPreferences.Editor editor;
-    private RadioButton rb_high;
-    private RadioButton rb_middle;
-    private RadioButton rb_low;
-    private RadioButton radioButton_4;
-    private RadioButton radioButton_5;
-    private Button gb_test;
-    private Button gb_unicode;
-    private Button gb_barcode;
-    private Button btnBmp;
-    private final static int ENABLE_RG = 10;
-    private final static int DISABLE_RG = 11;
+   
+   
     
-    TextView textViewMsg = null;
-    TextView textViewGray = null;
-    int ret = -1;
-    private boolean m_bThreadFinished = true;
-
-    private boolean is_cycle = false;
-    private int cycle_num = 0;
-
-    private int RESULT_CODE = 0;
-    private static final String DISABLE_FUNCTION_LAUNCH_ACTION = "android.intent.action.DISABLE_FUNCTION_LAUNCH";
-
-
-
-
-
-
+    
 
 
     /**
@@ -156,259 +114,53 @@ public class PrintActivity extends CordovaPlugin {
   
    
  
-    public void QuitHandler() {
-        is_cycle = false;
-        gb_test.setEnabled(true);
-        gb_barcode.setEnabled(true);
-        btnBmp.setEnabled(true);
-        gb_unicode.setEnabled(true);
-        handlers.removeCallbacks(runnable);
-    }
+   
 
 
-    Handler handlers = new Handler();
-    Runnable runnable = new Runnable() {
+   
 
-        @Override
-        public void run() {
-            // TODO Auto-generated method stub
 
-            Log.e(tag, "TIMER log...");
-            printThread = new Print_Thread(PRINT_UNICODE);
-            printThread.start();
 
-            Log.e(tag, "TIMER log2...");
-            if (RESULT_CODE == 0) {
-                editor = preferences.edit();
-                editor.putInt("count", ++cycle_num);
-                editor.commit();
-                Log.e(tag, "cycle num=" + cycle_num);
-                SendMsg("cycle num =" + cycle_num);
+
+
+   
+   int readNfcCard(){
+        Log.e("nfc", "heyp nfc Picc_Open start!");
+        byte[] NfcData_Len = new byte[5];
+        byte[] Technology = new byte[25];
+        byte[] NFC_UID = new byte[56];
+        byte[] NDEF_message = new byte[500];
+
+        int ret = posApiHelper.PiccNfc( NfcData_Len, Technology, NFC_UID, NDEF_message);
+
+        int TechnologyLength = NfcData_Len[0] & 0xFF;
+        int NFC_UID_length = NfcData_Len[1] & 0xFF;
+        int NDEF_message_length = (NfcData_Len[3] & 0xFF) + (NfcData_Len[4] & 0xFF);
+        byte[] NDEF_message_data = new byte[NDEF_message_length];
+        byte[] NFC_UID_data = new byte[NFC_UID_length];
+        System.arraycopy(NFC_UID, 0, NFC_UID_data, 0, NFC_UID_length);
+        System.arraycopy(NDEF_message, 0, NDEF_message_data, 0, NDEF_message_length);
+        String NDEF_message_data_str = new String(NDEF_message_data);
+        String NDEF_str = null;
+        if (!TextUtils.isEmpty(NDEF_message_data_str)) {
+            NDEF_str = NDEF_message_data_str.substring(NDEF_message_data_str.indexOf("en")+2,NDEF_message_data_str.length());
+        }
+
+        if (ret == 0) {
+            posApiHelper.SysBeep();
+            //successCount ++;
+            if (!TextUtils.isEmpty(NDEF_str)) {
+                textViewMsg.setText("TYPE: " + new String(Technology).substring(0, TechnologyLength) + "\n"
+                        + "UID: " + ByteUtil.bytearrayToHexString(NFC_UID_data, NFC_UID_data.length) + "\n"
+                        + NDEF_str);
+            }else{
+                textViewMsg.setText("TYPE: " + new String(Technology).substring(0, TechnologyLength) + "\n"
+                        + "UID: " + ByteUtil.bytearrayToHexString(NFC_UID_data, NFC_UID_data.length));
             }
-            handlers.postDelayed(this, 9000);
 
         }
-    };
-
-    Print_Thread printThread = null;
-
-    public class Print_Thread extends Thread {
-
-        String content = "1234567890";
-        int type;
-
-        public boolean isThreadFinished() {
-            return m_bThreadFinished;
-        }
-
-        public Print_Thread(int type) {
-            this.type = type;
-        }
-
-        public void run() {
-            Log.d("Print_Thread[ run ]", "run() begin");
-            Message msg = Message.obtain();
-            Message msg1 = new Message();
-
-            synchronized (this) {
-
-                m_bThreadFinished = false;
-                try {
-                    ret = posApiHelper.PrintInit();
-                } catch (PrintInitException e) {
-                    e.printStackTrace();
-                    int initRet = e.getExceptionCode();
-                    Log.e(tag, "initRer : " + initRet);
-                }
-
-                /*Log.e(tag, "init code:" + ret);
-
-                ret = getValue();
-                Log.e(tag, "getValue():" + ret);*/
-
-                posApiHelper.PrintSetGray(ret);
-
-//                posApiHelper.PrintSetVoltage(BatteryV * 2 / 100);
-
-                ret = posApiHelper.PrintCheckStatus();
-                if (ret == -1) {
-                    RESULT_CODE = -1;
-                    Log.e(tag, "Lib_PrnCheckStatus fail, ret = " + ret);
-                    SendMsg("Error, No Paper ");
-                    m_bThreadFinished = true;
-                    return;
-                } else if (ret == -2) {
-                    RESULT_CODE = -1;
-                    Log.e(tag, "Lib_PrnCheckStatus fail, ret = " + ret);
-                    SendMsg("Error, Printer Too Hot ");
-                    m_bThreadFinished = true;
-                    return;
-                } else if (ret == -3) {
-                    RESULT_CODE = -1;
-                    Log.e(tag, "voltage = " + (BatteryV * 2));
-                    SendMsg("Battery less :" + (BatteryV * 2));
-                    m_bThreadFinished = true;
-                    return;
-                }
-                /*
-                else if (voltage_level < 5) {
-                    RESULT_CODE = -1;
-                    Log.e(tag, "voltage_level = " + voltage_level);
-                    SendMsg("Battery capacity less : " + voltage_level);
-                    m_bThreadFinished = true;
-                    return;
-                }*/
-                else {
-                    RESULT_CODE = 0;
-                }
-
-                
-                        SendMsg("PRINT_TEST");
-                        msg.what = DISABLE_RG;
-                        handler.sendMessage(msg);
-
-                        //0 left，1 Ringht ，2 middle
-//                        Print.Lib_PrnSetAlign(0);
-                        posApiHelper.PrintSetFont((byte) 16, (byte) 16, (byte) 0x55);
-                        posApiHelper.PrintStr("AYA PAY POS SALES SLIP\n");
-                        posApiHelper.PrintSetFont((byte) 16, (byte) 16, (byte) 0x00);
-                        posApiHelper.PrintStr("CUSTOMER COPY\n");
-                        posApiHelper.PrintStr("- - - - - - - - - - - - - - - - - - - - - - - -\n");
-                        posApiHelper.PrintSetFont((byte) 24, (byte) 24, (byte) 0x00);
-                        posApiHelper.PrintStr("(MERCHANT NAME):\n");
-                        posApiHelper.PrintStr("中国银联直连测试\n");
-                        posApiHelper.PrintStr("商户编号(MERCHANT NO):\n");
-                        posApiHelper.PrintStr("    001420183990573\n");
-                        posApiHelper.PrintStr("终端编号(TERMINAL NO):00026715\n");
-                        posApiHelper.PrintStr("操作员号(OPERATOR NO):12345678\n");
-                        posApiHelper.PrintStr("- - - - - - - - - - - - - - - -\n");
-                        //  posApiHelper.PrintStr("\n");
-                        posApiHelper.PrintStr("发卡行(ISSUER):01020001 工商银行\n");
-                        posApiHelper.PrintStr("卡号(CARD NO):\n");
-                        posApiHelper.PrintStr("    9558803602109503920\n");
-                        posApiHelper.PrintStr("收单行(ACQUIRER):03050011民生银行\n");
-                        posApiHelper.PrintStr("交易类型(TXN. TYPE):消费/SALE\n");
-                        posApiHelper.PrintStr("卡有效期(EXP. DATE):2013/08\n");
-                        posApiHelper.PrintStr("- - - - - - - - - - - - - - - -\n");
-                        //  posApiHelper.PrintStr("\n");
-                        posApiHelper.PrintStr("批次号(BATCH NO)  :000023\n");
-                        posApiHelper.PrintStr("凭证号(VOUCHER NO):000018\n");
-                        posApiHelper.PrintStr("授权号(AUTH NO)   :987654\n");
-                        posApiHelper.PrintStr("日期/时间(DATE/TIME):\n");
-                        posApiHelper.PrintStr("    2008/01/28 16:46:32\n");
-                        posApiHelper.PrintStr("交易参考号(REF. NO):200801280015\n");
-                        posApiHelper.PrintStr("金额(AMOUNT):  RMB:2.55\n");
-                        posApiHelper.PrintStr("- - - - - - - - - - - - - - - -\n");
-                        //  posApiHelper.PrintStr("\n");
-                        posApiHelper.PrintStr("备注/REFERENCE\n");
-                        posApiHelper.PrintStr("- - - - - - - - - - - - - - - -\n");
-                        posApiHelper.PrintSetFont((byte) 16, (byte) 16, (byte) 0x00);
-                        posApiHelper.PrintStr("持卡人签名(CARDHOLDER SIGNATURE)\n");
-                        posApiHelper.PrintStr("\n");
-                        posApiHelper.PrintStr("- - - - - - - - - - - - - - - - - - - - - - - -\n");
-                        //  posApiHelper.PrintStr("\n");
-                        posApiHelper.PrintStr("  本人确认以上交易，同意将其计入本卡帐户\n");
-                        posApiHelper.PrintStr("  I ACKNOWLEDGE SATISFACTORY RECEIPT\n");
-                        posApiHelper.PrintStr("                                         ");
-                        posApiHelper.PrintStr("\n");
-                        posApiHelper.PrintStr("                                         ");
-                        posApiHelper.PrintStr("\n");
-                        posApiHelper.PrintStr("\n");
-                        posApiHelper.PrintStr("\n");
-                        posApiHelper.PrintStr("\n");
-
-                        SendMsg("Printing... ");
-                        ret = posApiHelper.PrintStart();
-
-                        msg1.what = ENABLE_RG;
-                        handler.sendMessage(msg1);
-
-                        Log.d("", "Lib_PrnStart ret = " + ret);
-
-                        if (ret != 0) {
-                            RESULT_CODE = -1;
-                            Log.e("liuhao", "Lib_PrnStart fail, ret = " + ret);
-                            if (ret == -1) {
-                                SendMsg("No Print Paper ");
-                            } else if(ret == -2) {
-                                SendMsg("too hot ");
-                            }else if(ret == -3) {
-                                SendMsg("low voltage ");
-                            }else{
-                                SendMsg("Print fail ");
-                            }
-                        } else {
-                            RESULT_CODE = 0;
-                            SendMsg("Print Finish ");
-                        }
-
-                
-                m_bThreadFinished = true;
-
-                Log.e(tag, "goToSleep2...");
-            }
-        }
+        return ret;
     }
-
-
-    public void SendMsg(String strInfo) {
-        Message msg = new Message();
-        Bundle b = new Bundle();
-        b.putString("MSG", strInfo);
-        msg.setData(b);
-        handler.sendMessage(msg);
-    }
-
-    private Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-
-            switch (msg.what) {
-                case DISABLE_RG:
-                    IsWorking = 1;
-                    rb_high.setEnabled(false);
-                    rb_middle.setEnabled(false);
-                    rb_low.setEnabled(false);
-                    radioButton_4.setEnabled(false);
-                    radioButton_5.setEnabled(false);
-                    break;
-
-                case ENABLE_RG:
-                    IsWorking = 0;
-                    rb_high.setEnabled(true);
-                    rb_middle.setEnabled(true);
-                    rb_low.setEnabled(true);
-                    radioButton_4.setEnabled(true);
-                    radioButton_5.setEnabled(true);
-
-                    break;
-                default:
-                    Bundle b = msg.getData();
-                    String strInfo = b.getString("MSG");
-                    textViewMsg.setText(strInfo);
-
-                    break;
-            }
-        }
-    };
-
-    public class BatteryReceiver extends BroadcastReceiver {
-        public void onReceive(Context context, Intent intent) {
-            voltage_level = intent.getExtras().getInt("level");// ��õ�ǰ����
-            Log.e("wbw", "current  = " + voltage_level);
-            BatteryV = intent.getIntExtra("voltage", 0);  //电池电压
-            Log.e("wbw", "BatteryV  = " + BatteryV);
-            Log.e("wbw", "V  = " + BatteryV * 2 / 100);
-            //  m_voltage = (int) (65+19*voltage_level/100); //放大十倍
-            //   Log.e("wbw","m_voltage  = " + m_voltage );
-        }
-    }
- /* public static Bitmap decodeBase64(String input)
-  {
-    byte[] decodedBytes = Base64.decode(input, 0);
-    return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
-  }*/
-   //This will send data to bluetooth printer
     boolean printText(CallbackContext callbackContext , JSONArray args) throws IOException {
         try {
                 try {
