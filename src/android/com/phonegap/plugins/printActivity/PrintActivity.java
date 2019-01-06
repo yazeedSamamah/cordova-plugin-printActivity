24ca1b49ce567d76ab1c9215386fd6f60ff87294
yazeedSamamah <yazeed_samamah@yahoo.com>
2019-01-06 12:21:04 +0200

    test

--- src/android/com/phonegap/plugins/printActivity/PrintActivity.java
+++ src/android/com/phonegap/plugins/printActivity/PrintActivity.java
@@ -187,7 +187,7 @@ public class PrintActivity extends CordovaPlugin {
         intent.setPackage("woyou.aidlservice.jiuiv5");
         intent.setAction("woyou.aidlservice.jiuiv5.IWoyouService");*/
 
-woyouService.printerSelfChecking(callback);//这里使用的AIDL方式打印
+woyouService.printtext (" yazeed", callback);
  return true;
         }
 
